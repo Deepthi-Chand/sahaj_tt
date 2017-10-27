1759ccedb4434f1c07b770d1f33287eefa129b10
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-10-27 20:30:07 +0530

    Fix #9

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react';
 import { StatelessComponent } from 'react';
 import { Props } from '../containers/UserInfo';
+import { Link } from 'react-router-dom';
 
 export const UserInfo: StatelessComponent<Props> = ({ authenticated, name }) =>
-  authenticated ? <p>{name}</p> : null;
+  authenticated
+    ? (
+      <nav>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/matches'>Matches</Link></li>
+          <li><Link to='/registrations'>Registrations</Link></li>
+          <li><Link to='/admin/registations'>Admin</Link></li>
+          <li><p>{name}</p></li>
+        </ul>
+      </nav>
+    )
+    : null;
