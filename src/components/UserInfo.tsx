a9b0e94204aab02e7c8dcda304adefb5fa146790
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-10-28 18:48:18 +0530

    Use NavButtons and Buttons

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -2,18 +2,18 @@ import * as React from 'react';
 import { StatelessComponent } from 'react';
 import { Props } from '../containers/UserInfo';
 import { Link } from 'react-router-dom';
+import { NavButton } from './NavButton';
+import { Button } from 'material-ui';
 
 export const UserInfo: StatelessComponent<Props> = ({ authenticated, name, admin }) =>
   authenticated
     ? (
-      <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/matches'>Matches</Link></li>
-          <li><Link to='/registrations'>Registrations</Link></li>
-          { admin && <li><Link to='/admin/registrations'>Admin</Link></li> }
-          <li><p>{name}</p></li>
-        </ul>
-      </nav>
+      <div>
+        <NavButton color='contrast' to='/'>Home</NavButton>
+        <NavButton color='contrast' to='/matches'>Matches</NavButton>
+        <NavButton color='contrast' to='/registrations'>Registrations</NavButton>
+        { admin && <NavButton color='contrast' to='/admin/registrations'>Admin</NavButton> }
+        <Button disabled color='contrast'>{name}</Button>
+      </div>
     )
     : null;
