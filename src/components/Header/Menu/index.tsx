b3c436462b312396c9efe56d61b346a9a07672e3
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-10-28 21:50:19 +0530

    Apply theme to Menu in Header

--- src/components/Header/Menu/index.tsx
+++ src/components/Header/Menu/index.tsx
@@ -3,16 +3,17 @@ import { StatelessComponent } from 'react';
 import { UserMenu } from './UserMenu';
 import { Link } from 'react-router-dom';
 import { NavButton } from 'components/NavButton';
-import { Button } from 'material-ui';
+import { Button, WithStyles, withStyles } from 'material-ui';
 import { connect } from 'react-redux';
 import { State } from 'store/types';
+import { Theme } from 'material-ui/styles';
 
 interface StateProps {
   authenticated: boolean;
   admin: boolean;
 }
 
-export interface Props extends StateProps {
+export interface Props extends StateProps, Partial<WithStyles<'root'>> {
 
 }
 
@@ -21,10 +22,19 @@ const mapStateToProps = ({ authentication: { authenticated, admin }}: State): St
   admin
 });
 
-export const MenuComponent: StatelessComponent<Props> = ({ authenticated, admin }) =>
+const styles = (theme: Theme) => ({
+  root: {
+    display: 'flex',
+    '& > div': {
+      display: 'inline-flex'
+    }
+  }
+});
+
+const MenuComponent: StatelessComponent<Props> = ({ authenticated, admin, classes }) =>
   authenticated
     ? (
-      <div>
+      <div className={classes.root}>
         <NavButton color='contrast' to='/'>Home</NavButton>
         <NavButton color='contrast' to='/matches'>Matches</NavButton>
         <NavButton color='contrast' to='/registrations'>Registrations</NavButton>
@@ -34,4 +44,4 @@ export const MenuComponent: StatelessComponent<Props> = ({ authenticated, admin 
     )
     : null;
 
-export const Menu = connect(mapStateToProps)(MenuComponent);
+export const Menu = connect(mapStateToProps)(withStyles(styles)(MenuComponent));
