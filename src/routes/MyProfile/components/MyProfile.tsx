1fb4b99ca53c603fee7830f13dcb44fb6e97cf16
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-10-29 18:36:17 +0530

    Use container and get rid of mock props

--- src/routes/MyProfile/components/MyProfile.tsx
+++ src/routes/MyProfile/components/MyProfile.tsx
@@ -1,65 +1,16 @@
 import * as React from 'react';
 import { StatelessComponent } from 'react';
 import { Grid, Paper, Typography, Divider } from 'material-ui';
-import { PendingResults } from 'routes/MyProfile/components/PendingResults';
-import { Match } from 'api';
-const matches: Match[] = [
-  {
-    "id": "1",
-    "team_one": {
-      "id": "team-1",
-      "player_one": "deepthichand@sahajsoft.com"
-    },
-    "team_two":{
-      "id": "team-2",
-      "player_one": "shalin@sahajsoft.com"
-    },
-    "completed": true,
-    "date": new Date()
-  },
-  {
-    "id": "2",
-    "team_one": {
-      "id": "team-1",
-      "player_one": "deepthichand@sahajsoft.com"
-    },
-    "team_two":{
-      "id": "team-2",
-      "player_one": "shalin@sahajsoft.com"
-    },
-    "completed": false,
-    "date": new Date()
-  },
-  {
-    "id": "3",
-    "team_one": {
-      "id": "team-1",
-      "player_one": "deepthichand@sahajsoft.com"
-    },
-    "team_two":{
-      "id": "team-2",
-      "player_one": "shalin@sahajsoft.com"
-    },
-    "completed": false,
-    "date": new Date()
-  }
-];
+import { MyMatches } from './MyMatches';
+import { MyProfileProps } from '../containers/MyProfile';
 
-interface MyProfileProps {
-  email: string;
-}
-
-export const MyProfile: StatelessComponent<MyProfileProps> = ({ email }) =>
-  <Grid container alignItems='stretch'>
-    <Grid item xs={12} md={8}>
-      <Typography type='headline'>My Profile</Typography>
+export const MyProfile: StatelessComponent<MyProfileProps> = ({ name, email, matches }) =>
+  <Grid container alignItems='stretch' justify='center'>
+    <Grid item xs={12} sm={8}>
+      <Typography type='headline'>{name}</Typography>
       <Typography type='subheading'>{email}</Typography>
       <Divider />
-    </Grid>
-    <Grid item xs={12} md={4}>
-      <Paper>
-        <Typography type='headline'>Your matches with pending results</Typography>
-        <PendingResults matches={matches} />
-      </Paper>
+      <Typography type='body2'>Your matches</Typography>
+      <MyMatches matches={matches} />
     </Grid>
   </Grid>;
