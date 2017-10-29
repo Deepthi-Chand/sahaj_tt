c0555bbffa4113b2a61d90999d7413ce13788090
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-10-29 21:05:51 +0530

    Pass on function to test if a match is a pending loss for current user

--- src/routes/MyProfile/containers/MyProfile.ts
+++ src/routes/MyProfile/containers/MyProfile.ts
@@ -8,6 +8,7 @@ interface StateProps {
   email: string;
   name: string;
   matches: Match[];
+  isPendingLoss: (match: Match) => boolean;
 }
 
 interface DispatchProps {
@@ -25,11 +26,23 @@ export interface MyProfileProps extends StateProps, DispatchProps, OwnProps {
 
 const isPartOfMatch = (email: string) =>
   (match: Match) => match.team_one.player_one === email || match.team_two.player_one === email;
+const isResultPending = ({ result }: Match) => !!result && !result.confirmed;
+const getPlayerTeamId = (email: string) =>
+  (match: Match) =>
+    isPartOfMatch(email)(match)
+      ? match.team_one.player_one === email ? match.team_one.id : match.team_two.id
+      : undefined;
+const isTeamLosing = (team_id: string) =>
+  (match: Match) =>
+    (match.team_one.id === team_id || match.team_two.id === team_id) && match.result.winning_team !== team_id;
+const isPendingLoss = (email: string) =>
+  (match: Match) => isResultPending(match) && isTeamLosing(getPlayerTeamId(email)(match))(match);
 
 const mapStateToProps = ({ authentication: { email, name }, matches: { items } }: State): StateProps => ({
   email,
   name,
-  matches: items.filter(isPartOfMatch(email))
+  matches: items.filter(isPartOfMatch(email)),
+  isPendingLoss: isPendingLoss(email)
 });
 
 const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
