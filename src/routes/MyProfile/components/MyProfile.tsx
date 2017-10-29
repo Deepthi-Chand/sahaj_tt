b066e68f700cb2968131ea0b20b1c2b83869dd2c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-10-29 18:59:51 +0530

    Allow updating result from My Matches part of My Profile

--- src/routes/MyProfile/components/MyProfile.tsx
+++ src/routes/MyProfile/components/MyProfile.tsx
@@ -4,13 +4,13 @@ import { Grid, Paper, Typography, Divider } from 'material-ui';
 import { MyMatches } from './MyMatches';
 import { MyProfileProps } from '../containers/MyProfile';
 
-export const MyProfile: StatelessComponent<MyProfileProps> = ({ name, email, matches }) =>
+export const MyProfile: StatelessComponent<MyProfileProps> = ({ name, email, matches, updateResult, confirmResult }) =>
   <Grid container alignItems='stretch' justify='center'>
     <Grid item xs={12} sm={8}>
       <Typography type='headline'>{name}</Typography>
       <Typography type='subheading'>{email}</Typography>
       <Divider />
       <Typography type='body2'>Your matches</Typography>
-      <MyMatches matches={matches} />
+      <MyMatches matches={matches} updateResult={updateResult} confirmResult={confirmResult} />
     </Grid>
   </Grid>;
