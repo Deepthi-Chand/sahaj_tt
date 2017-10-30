891a46aedc8e7d811add0aeddffe0c1dcd025a24
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-10-30 19:24:18 +0530

    Export only createApi and Api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,13 @@
 import { getFetch } from './fetch';
-import { createMatches } from './matches';
-import { createRegistrations } from './registrations';
-import { API_URL } from 'globals';
+import { createMatches, MatchesApi } from './matches';
+import { createRegistrations, RegistrationsApi } from './registrations';
 
-const createApi = (baseUri: string) => {
+export interface Api {
+  matches: MatchesApi;
+  registrations: RegistrationsApi;
+}
+
+export const createApi = (baseUri: string): Api => {
   const fetch = getFetch(baseUri);
   return  {
     matches: createMatches(fetch),
@@ -14,8 +18,3 @@ const createApi = (baseUri: string) => {
 export { User } from './users';
 export { Match } from './matches';
 export { Registration } from './registrations';
-
-export const {
-  matches,
-  registrations
-} = createApi(API_URL);
